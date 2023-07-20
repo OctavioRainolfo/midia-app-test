3b04b2edf03415bdce2fb481410fa6bb68fba187
Octavio <octaviorainolfo@gmail.com>
2023-07-20 15:06:23 -0300

    working demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,43 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { apiMidia, apiUsuario } from './api';
+import { postImage, getMidias, deleteImage, getAllImages, postImageName, getUser, patchName } from './api';
 import './App.css';
 
 interface ImageInfo {
-    arquivoId: number;
+    arquivoId?: number;
     nome: string;
-    tipo: string;
+    tipo?: string;
     url: string;
-    dataCriacao: string;
-    dataAtualizacao: string;
-    deletado: boolean;
+    dataCriacao?: string;
+    dataAtualizacao?: string;
+    deletado?: boolean;
 }
 
 
 function ImageManagementDemo() {
     const [images, setImages] = useState<ImageInfo[]>([]);
-    const [selectedImage, setSelectedImage] = useState<ImageInfo | null>(null);
+    const [selectedImage, setSelectedImage] = useState<ImageInfo | undefined | null>();
     const [uploadFile, setUploadFile] = useState<File | null>(null);
-    const [fileName, setFileName] = useState('');
-    const [userCode, setuserCode] = useState(localStorage.getItem('userCode') || '');
-    const [downloadedImages, setDownloadedImages] = useState<string>('');
+    const [uploadFileName, setUploadFileName] = useState('');
+    const [editFileName, setEditFileName] = useState('');
+    const [userCode, setUserCode] = useState('');
+    const [downloadedImages, setDownloadedImages] = useState<ImageInfo[]>([]);
+    const [toggleEdit, setToggleEdit] = useState<boolean[]>([]);
+    const [updateImages, setUpdateImages] = useState(false);
 
     const imgRef = useRef<HTMLImageElement | null>(null);
 
-    const validateUser = () => {
-        // Check if user exists
-        apiUsuario.get(`validarUsuario/` + userCode)
-            .then(response => {
-                if (response.data) {
-                    searchImages();
-                }
-                if (!response.data) {
-                    // If user does not exist, create user
-                    apiUsuario.post(`salvarUsuario/${userCode}`)
-                        .then(response => {
-                            if (response.status === 200) {
-                                localStorage.setItem('userCode', userCode);
-                            } else {
-                                alert('Error saving user. Please try again.');
-                            }
-                        });
-                }
-                localStorage.setItem('userCode', userCode);
-            });
+    const validateUser = (code?: string) => {
+        const verifyCode = code ? code : userCode;
+        getUser(verifyCode, setImages);
     }
 
-    const searchImages = () => {
-        apiMidia.get(`buscaArquivosUsuario/${userCode}`)
-            .then((response) => {
-                console.log('response', response);
-                if (response.data) {
-                    const imageResponse = response.data;
-                    const filteredImages = imageResponse.filter((image: ImageInfo) => !image.deletado);
-                    setImages(filteredImages);
-                    localStorage.setItem('images', JSON.stringify(filteredImages));
-                } else {
-                    alert('No images found for this user.');
-                }
-            })
+    const searchImages = async (code?: string) => {
+        const verifyCode = code ? code : userCode;
+        if (!verifyCode) return;
+        await getMidias(verifyCode, setImages).then(() => {
+            getAllImages(verifyCode, images, setDownloadedImages);
+        }
+        );
     }
 
     const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -66,26 +46,64 @@ function ImageManagementDemo() {
         }
     }
 
+    const handleDownloadImages = async () => {
+        getAllImages(userCode, images, setDownloadedImages)
+    }
+
+    const searchByName = (image: ImageInfo) => {
+        var temp = downloadedImages.find((img) => img.nome === image.nome);
+        return temp;
+    }
+
+    const searchByUrl = (image: ImageInfo) => {
+        var tempDownloaded = downloadedImages.find((img) => img.url === image.url);
+        var temp = images.find((img) => img.nome === tempDownloaded?.nome);
+        return temp;
+    }
+
+    const searchById = (image: ImageInfo) => {
+        var temp = images.find((img) => img.arquivoId === image.arquivoId);
+        return temp;
+    }
+
+    const searchDownloadedById = (image: ImageInfo) => {
+        var temp = downloadedImages.find((img) => img.arquivoId === image.arquivoId);
+        return temp;
+    }
+
+    const handleFindImage = (image: ImageInfo) => {
+        const temp = downloadedImages.find((img) => img.arquivoId === image.arquivoId);
+        return temp?.url;
+    }
+
+    const handleToggleEdit = (index: number) => {
+        setEditFileName(images[index].nome);
+        setToggleEdit((prevToggleEdit) => {
+            const updatedToggleEdit = [...prevToggleEdit];
+            updatedToggleEdit[index] = !updatedToggleEdit[index];
+            return updatedToggleEdit;
+        });
+    };
+
+    const handleEditName = (image: ImageInfo, index: number) => {
+        var searchedId = searchById(image);
+        if (searchedId) {
+            patchName(userCode, searchedId.url, setImages, editFileName)
+            handleToggleEdit(index);
+        }
+    };
+
     const uploadImage = () => {
         if (uploadFile) {
             let formData = new FormData();
             formData.append('file', uploadFile);
-
             if (userCode) {
-                apiMidia.post(`salvarArquivo/${userCode}`, formData)
-                    .then((response: { status: number; }) => {
-                        if (response.status === 200) {
-                            alert('Image uploaded successfully!');
-                        }
-                        apiMidia.get(`buscaArquivosUsuario/${userCode}`)
-                            .then((response) => {
-                                setImages(response.data);
-                                localStorage.setItem('images', JSON.stringify(response.data));
-                            });
-                    })
+                postImage(userCode, formData, setImages);
             } else {
                 alert('Please enter user code.');
             }
+        } else {
+            alert('Please select an image.');
         }
     }
 
@@ -93,132 +111,158 @@ function ImageManagementDemo() {
         if (uploadFile) {
             let formData = new FormData();
             formData.append('file', uploadFile);
-
-            apiMidia.post(`salvarArquivoNome/${fileName}/${userCode}`, formData)
-                .then((response: { status: number; }) => {
-                    if (response.status === 200) {
-                        alert('Image with name uploaded successfully!');
-                        searchImages();
-                    }
-                    apiMidia.get(`buscaArquivosUsuario/${userCode}`)
-                        .then((response) => {
-                            setImages(response.data);
-                            localStorage.setItem('images', JSON.stringify(response.data));
-                        });
-                })
+            if (userCode) {
+                console.log(userCode, uploadFile);
+                postImageName(userCode, formData, uploadFileName, setImages)
+            }
         }
     }
 
-    const downloadImage = (url: string) => {
-        apiMidia.get(`baixarArquivo/` + url + "/" + userCode, { responseType: 'blob' })
-            .then((response: { data: BlobPart; }) => {
-                let link = document.createElement('a');
-                link.href = window.URL.createObjectURL(new Blob([response.data]));
-                link.download = 'image.jpg';
-                link.click();
-            });
-    }
-
-    const fileDownloadHandler = (url: string) => {
-        console.log('url', url);
-        apiMidia.get(`verArquivo/${url}/${userCode}`, {
-            responseType: 'arraybuffer' // informe ao axios que estamos esperando dados binários
-        }).then((response) => {
-
-            // Criar um Blob a partir dos dados binários
-            const blob = new Blob([response.data], { type: 'image/png' });
-
-            // Criar um URL Object a partir do Blob
-            const imageUrl = URL.createObjectURL(blob);
-
-            // Atualizar o estado com a URL da imagem
-            setDownloadedImages(imageUrl);
-        });
-    }
-
     //useRef to get the image and scroll to it
-    const selectAndScroll = (image: ImageInfo) => {
-        fileDownloadHandler(image.url);
-        setSelectedImage(image);
-        const img = imgRef.current;
-        if (img) {
-            img.scrollIntoView({ behavior: 'smooth' });
+    const selectAndScroll = async (image: ImageInfo) => {
+        var searchedId = searchDownloadedById(image);
+        console.log(searchedId);
+        if (searchedId) {
+            setSelectedImage(searchedId);
+            const img = imgRef.current;
+            if (img) {
+                img.scrollIntoView({ behavior: 'smooth' });
+            }
         }
     }
 
     //get the url of the image and delete it
     const handleDeleteImage = (image: ImageInfo) => {
-        apiMidia.delete(`deletarArquivo/${image.url}/${userCode}`)
-            .then((response) => {
-                if (response.status === 200) {
-                    alert('Image deleted successfully!');
-                    searchImages();
-                }
-            })
+        var imageId = searchById(image);
+        console.log(imageId);
+        if (imageId) {
+            deleteImage(userCode, imageId.url, setImages);
+        }
     }
 
+    useEffect(() => {
+        handleDownloadImages();
+        console.log('downloadedImages')
+    }, [images])
+
+    useEffect(() => {
+        if (downloadedImages.length > 0) {
+            setSelectedImage(downloadedImages[0]);
+        }
+    }, [downloadedImages])
+
+    useEffect(() => {
+        var userCode = localStorage.getItem('userCode');
+        if (userCode) {
+            setUserCode(userCode);
+            validateUser(userCode);
+        }
+    }, [userCode]);
+
+    useEffect(() => {
+        if (updateImages) {
+            searchImages();
+            setUpdateImages(false);
+            console.log('update');
+        }
+    }, [updateImages]);
 
     return (
         <div className="container">
             <div className="image-management-demo">
-                <h2>Image Management Demo</h2>
 
-                <div className="upload-section">
+                <div className="upload-section box-container">
                     <div className='upload-form'>
-                        <h3>User Code</h3>
-                        <input type="text" placeholder="Enter user code" value={userCode} onChange={e => setuserCode(e.target.value)} />
-                        {userCode && (
-                            <button onClick={() => {
-                                validateUser();
-                            }}>Search Images</button>
-                        )}
+                        <h2>Image Management Demo</h2>
 
-                        <h3>Upload Image</h3>
-                        <input type="file" onChange={handleFileUpload} />
-                        <button onClick={uploadImage}>Upload</button>
-                        <input type="text" placeholder="Enter file name" value={fileName} onChange={e => setFileName(e.target.value)} />
-                        <button onClick={uploadImageWithName}>Upload with name</button>
+                        <div className='upload-container'>
+
+                            <div className='userCode-form'>
+                                <h3>User Code</h3>
+                                <input type="text" placeholder="Enter user code" value={userCode} onChange={e => setUserCode(e.target.value)} />
+                                <button onClick={() => {
+                                    validateUser();
+                                }}>Search Images</button>
+                            </div>
+
+                            <div className='uploadImage-form'>
+                                <h3>Upload Image</h3>
+                                <div className='box-input'>
+                                    <input type="file" onChange={handleFileUpload} />
+                                    <button onClick={uploadImage}>Upload</button>
+                                </div>
+                                <div className='box-input'>
+                                    <input type="text" placeholder="Enter file name" value={uploadFileName} onChange={e => setUploadFileName(e.target.value)} />
+                                    <button onClick={uploadImageWithName}>Upload with name</button>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
 
-                <div className="images-section">
-                    <h3>Images</h3>
-                    {images.map((image, index) => (
-                        <div key={index} className="image-card">
-                            <div className='image-info-section' onClick={() =>
-                                selectAndScroll(image)
-                            }>
-                                <p>
-                                    {image.nome}
-                                </p>
-                                <p>
-                                    {image.url}
-                                </p>
-                            </div>
-                            <div className='buttons-section'>
-                                <button onClick={() =>
-                                    selectAndScroll(image)
-                                }>Download</button>
-                                <button onClick={
-                                    () => handleDeleteImage(image)
-                                }>Delete</button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
+                <div className='images-container'>
+                    <div className="images-section">
+                        <h3>Images</h3>
+                        <div className='map-cards-container'>
+                            {downloadedImages.map((image, index) => (
+                                <div key={index} className="image-card">
+                                    <div className='image-info-section'>
 
-                {selectedImage && (
+                                        <div className='wrap-mini-image'>
+
+                                            <div className='mini-image'>
+                                                <img src={
+                                                    handleFindImage(image)
+                                                } alt={image.nome}
+                                                    onClick={() =>
+                                                        selectAndScroll(image)
+                                                    }
+                                                />
+                                            </div>
+
+                                            {toggleEdit[index] ? (
+                                                <div className='wrap-input'>
+                                                    <input type="text" value={editFileName} onChange={e => setEditFileName(e.target.value)} />
+                                                    <button onClick={() => {
+                                                        handleEditName(image, index);
+                                                    }}>Save</button>
+                                                </div>
+                                            ) : (
+                                                <p>{image.nome}</p>
+                                            )}
+                                        </div>
+
+                                    </div>
+                                    <div className='buttons-section'>
+                                        <button onClick={() =>
+                                            handleToggleEdit(index)
+                                        }>Edit</button>
+                                        <button onClick={
+                                            () => handleDeleteImage(image)
+                                        }>Delete</button>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
                     <div className="info-section">
                         <h3>Image Information</h3>
-                        <p><strong>URL:</strong> {selectedImage.url}</p>
-                        <p>{downloadedImages &&
-                            <img ref={imgRef}
-                                className='downloadedImage' src={downloadedImages} />
-                        }</p>
+                        <p><strong>Nome:</strong> {selectedImage?.nome}</p>
+
+                        <div className='downloaded-image'>
+                            {selectedImage && <img
+                                className="selected-image"
+                                src={selectedImage.url} alt="downloaded"
+                                ref={imgRef}
+                            />}
+                        </div>
+
                     </div>
-                )}
+                </div>
             </div>
-        </div>
+        </div >
     );
 }
 
--- src/api.tsx
+++ src/api.tsx
@@ -1,4 +1,15 @@
 import axios from 'axios';
+import { Dispatch, SetStateAction } from 'react';
+
+interface ImageInfo {
+    arquivoId?: number;
+    nome: string;
+    tipo?: string;
+    url: string;
+    dataCriacao?: string;
+    dataAtualizacao?: string;
+    deletado?: boolean;
+}
 
 export const apiMidia = axios.create({
     baseURL: "https://localhost:7111/api/v1/Midia/",
@@ -7,3 +18,164 @@ export const apiMidia = axios.create({
 export const apiUsuario = axios.create({
     baseURL: "https://localhost:7111/api/v1/Usuario/",
 });
+
+
+//Get's from Midia API
+
+export const getMidias = async (verifyCode: string, callback: (images: ImageInfo[]) => void) => {
+    apiMidia.get(`buscaArquivosUsuario/${verifyCode}`)
+        .then((response) => {
+            if (response.data) {
+                const imageResponse = response.data;
+                const filteredImages = imageResponse.filter((image: ImageInfo) => !image.deletado);
+                callback(filteredImages);
+            } else {
+                alert('No images found for this user.');
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+export const getSingleImage = async (
+    image: ImageInfo,
+    userCode: string,
+): Promise<ImageInfo> => {
+    return new Promise((resolve, reject) => {
+        apiMidia.get(`verArquivo/${image.url}/${userCode}`, {
+            responseType: 'arraybuffer' // informe ao axios que estamos esperando dados binários
+        }).then((response) => {
+
+            // Criar um Blob a partir dos dados binários
+            const blob = new Blob([response.data], { type: 'image/png' });
+            // Criar um URL Object a partir do Blob
+            const imageUrl = {
+                url: URL.createObjectURL(blob),
+                nome: image.nome,
+                arquivoId: image.arquivoId,
+            }
+            resolve(imageUrl);
+        })
+            .catch((error) => {
+                reject(error);
+            });
+    }
+    )
+}
+
+export const getAllImages = async (
+    userCode: string,
+    array: ImageInfo[],
+    callback: (image: ImageInfo[]) => void
+) => {
+    try {
+        const newArray = await Promise.all(array.map((image) => getSingleImage(image, userCode)));
+        callback(newArray);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+// Get's from Usuario API
+
+export const getUser = async (verifyCode: string, callback: (user: any) => void) => {
+    apiUsuario.get(`validarUsuario/` + verifyCode)
+        .then(response => {
+            if (response.data) {
+                getMidias(verifyCode, callback);
+            }
+            if (!response.data) {
+                // If user does not exist, create user
+                apiUsuario.post(`salvarUsuario/${verifyCode}`)
+                    .then(response => {
+                        if (response.status === 200) {
+                            localStorage.setItem('userCode', verifyCode);
+                        } else {
+                            alert('Error saving user. Please try again.');
+                        }
+                    });
+            }
+            localStorage.setItem('userCode', verifyCode);
+        });
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+// Post's from Midia API
+
+export const postImage = async (verifyCode: string,
+    formData: FormData,
+    callback: (images: ImageInfo[]) => void
+) => {
+    apiMidia.post(`salvarArquivo/${verifyCode}`, formData)
+        .then((response: { status: number; }) => {
+            if (response.status === 200) {
+                alert('Image uploaded successfully!');
+                getMidias(verifyCode, callback);
+            }
+        }).catch((error) => {
+            console.log(error);
+        });
+}
+
+export const postImageName = async (verifyCode: string,
+    formData: FormData,
+    fileName: string,
+    callback: (images: ImageInfo[]) => void
+) => {
+    apiMidia.post(`salvarArquivoNome/${fileName}/${verifyCode}`, formData)
+        .then((response: { status: number; }) => {
+            if (response.status === 200) {
+                alert('Image with name uploaded successfully!');
+                getMidias(verifyCode, callback);
+            }
+        }).catch((error) => {
+            console.log(error);
+        });
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// Patch's from Midia API
+
+export const patchName = async (verifyCode: string,
+    url: string,
+    callback: (user: any) => void,
+    newName: string) => {
+
+    console.log(newName);
+    await apiMidia.patch(`alterarNomeArquivo/${url}/${verifyCode}`, JSON.stringify(newName), {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        if (response.status === 200) {
+            getMidias(verifyCode, callback);
+        } else {
+            alert('Error saving user. Please try again.');
+        }
+    }).catch((error) => {
+        console.log(error);
+    }
+    );
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////
+// Delete's from Midia API
+
+export const deleteImage = async (verifyCode: string,
+    url: string,
+    callback: (image: ImageInfo[]) => void
+    ) => {
+    apiMidia.delete(`deletarArquivo/${url}/${verifyCode}`)
+        .then((response) => {
+            if (response.status === 200) {
+                alert('Image deleted successfully!');
+                getMidias(verifyCode, callback);
+            }
+        }).catch((error) => {
+            console.log(error);
+        });
+}
